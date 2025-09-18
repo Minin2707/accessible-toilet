c4bcad97045d3b45329ed039dd76954de62b8805
Minin2707 <nesta.r@yandex.ru>
2025-09-18 16:20:45 +0300

    initial commit

---
+++ src/main/java/accessible_toilet/accessible_toilet/AccessibleToiletApplication.java
@@ -0,0 +1,13 @@
+package accessible_toilet.accessible_toilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessibleToiletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessibleToiletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/accessible_toilet/accessible_toilet/AccessibleToiletApplicationTests.java
@@ -0,0 +1,13 @@
+package accessible_toilet.accessible_toilet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessibleToiletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
